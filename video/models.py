e4422d6e1fa2b8faacbe8efd2dcf3d71e8785ecf
omoknooni <hihm0609@gmail.com>
2024-03-29 00:30:25 +0900

    add: model Caption

--- video/models.py
+++ video/models.py
@@ -14,3 +14,15 @@ class Video(models.Model):
     
     def get_absolute_url(self):
         return reverse('video-detail', args=[str(self.id)])
+    
+
+class Caption(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    frame_number = models.IntegerField()
+    sentence = models.TextField()
+    detected_object = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
